da7b8e36b4b83870efc2ec24bfd4c131286ae99e
Marzya <mnaq_1987@yahoo.com>
2025-03-20 00:49:01 +0330

    created test.html and ...
    created test page and displayed posts in this page with "for" and "if" tags.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def blog_view(request):
@@ -7,3 +8,8 @@ def blog_view(request):
 
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
+
+def test(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request,'test.html', context)
