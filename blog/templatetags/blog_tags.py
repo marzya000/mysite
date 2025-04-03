c999975add64c4e8984206492740fe8c91088ed7
Marzya <mnaq_1987@yahoo.com>
2025-04-03 23:17:41 +0330

    latestposts in sidebar
    we displayed three latest posts in Popular Post section.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -17,7 +17,7 @@ def function():
 def snippet(value,arg=20):
     return value[:arg] + "..."
 
-@register.inclusion_tag('popularposts.html')
-def popularposts():
-    posts = Post.objects.filter(status=1).order_by('published_date')[:1]
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts':posts}
