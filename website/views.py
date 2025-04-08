d9344e5d7e868054dd794cb5beac773598022129
Marzya <mnaq_1987@yahoo.com>
2025-04-08 23:18:32 +0330

    create form in templates
    we created a contact form with form tag in template test.

--- website/views.py
+++ website/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from website.models import Contact
+
 
 
 def index_view(request):
@@ -10,7 +13,23 @@ def about_view(request):
 def contact_view(request):
     return render(request, 'website/contact.html')
 
+def test_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        c = Contact()
+        c.name = name
+        c.email = email
+        c.subject = subject
+        c.message = message
+        c.save()
+        print(name,email,subject,message)
+        
 
+
+    return render(request, 'test.html',{})
 
 
 
