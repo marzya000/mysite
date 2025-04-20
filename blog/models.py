fba063cd1bc01de3530b7614a014f66dcb0d40c4
Marzya <mnaq_1987@yahoo.com>
2025-04-20 23:38:27 +0330

    sign up form
    created sign up form.created its url and view.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from taggit.managers import TaggableManager
+from taggit.managers import TaggableManager # type: ignore
 
 
 # Create your models here.
